6390061dd3e7c39d0a417d6b1d144eeee9a3e8c2
Агаларов Игорь <igorvippzone@gmail.com>
2023-07-21 13:06:10 +0300

    update sidebar, add button themes

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -5,25 +5,46 @@ import cls from './Button.module.scss';
 export enum ThemeButton {
     CLEAR = 'clear',
     OUTLINE = 'outline',
+    BACKGROUND = 'background',
+    BACKGROUND_INVERTED = 'backgroundInverted'
 }
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+export enum ButtonSize {
+    M = 'size_m',
+    L = 'size_l',
+    XL = 'size_xl'
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string;
     theme?: ThemeButton;
+    square?: boolean,
+    size?: ButtonSize,
 }
 
 export const Button: FC<ButtonProps> = (props) => {
     const {
         className,
         children,
-        theme,
+        theme = ThemeButton.OUTLINE,
+        square,
+        size = ButtonSize.M,
         ...otherProps
     } = props;
+
+    const mods: Record<string, boolean> = {
+        [cls.square]: square,
+    };
+    const additional: string[] = [
+        cls[theme],
+        cls[size],
+        className,
+    ];
 
     return (
         <button
             type="button"
-            className={classNames(cls.Button, { [cls[theme]]: true }, [className])}
+            className={classNames(cls.Button, mods, additional)}
             {...otherProps}
         >
             {children}
